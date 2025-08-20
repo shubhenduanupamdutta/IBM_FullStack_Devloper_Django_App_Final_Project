47fb7fbd6e9b6932026cf2a1be9648d5bbc6cd75
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-08-20 21:49:22 +0530

    exam result view

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -144,12 +144,32 @@ def extract_answers(request: HttpRequest) -> list[int]:
             submitted_answers.append(choice_id)
     return submitted_answers
 
+def show_exam_result(request: HttpRequest, course_id: int, submission_id: int) -> HttpResponse:
+    context = {}
+    course = get_object_or_404(Course, pk=course_id)
+    submission = Submission.objects.get(id=submission_id)
+    choices = submission.choices.all()
 
-# <HINT> Create an exam result view to check if learner passed exam and show their question results
-# and result for each question,
-# you may implement it based on the following logic:
-# Get course and submission based on their ids
-# Get the selected choice ids from the submission record
-# For each selected choice, check if it is a correct answer or not
-# Calculate the total score
-# def show_exam_result(request, course_id, submission_id):
+    total_score = 0
+    # Assuming course has related questions
+    questions = course.question_set.all()  # pyright: ignore[reportAttributeAccessIssue]
+
+    for question in questions:
+        correct_choices = question.choice_set.filter(
+            is_correct=True,
+        )  # Get all correct choices for the question
+        selected_choices = choices.filter(
+            question=question,
+        )  # Get the user's selected choices for the question
+
+        # Check if the selected choices are the same as the correct choices
+        if set(correct_choices) == set(selected_choices):
+            total_score += (
+                question.grade
+            )  # Add the question's grade only if all correct answers are selected
+
+    context["course"] = course
+    context["grade"] = total_score
+    context["choices"] = choices
+
+    return render(request, "onlinecourse/exam_result_bootstrap.html", context)
